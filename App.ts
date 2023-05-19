e0dc91d12f540898a525790d726376b753da8e8a
Ayy <ayy@Ayys-MacBook-Pro.local>
2023-05-20 00:47:54 +1000

    initial setup

---
+++ App.ts
@@ -0,0 +1,17 @@
+// Libraries
+import WebSocket, { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({
+  port: 8080,
+})
+
+wss.on('connection', (ws) => {
+  ws.on('error', console.error);
+  
+  ws.on('message', (data) => {
+    console.log('received: %s', data )
+  })
+  
+  ws.send('connection opened')
+
+})
---
+++ src/cards/cards.ts
@@ -0,0 +1,12 @@
+
+// c = clubs | d = diamonds | h = hearts | s = spades
+const Deck = [
+  'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck',
+  'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk',
+  's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk',
+]
+
+const shuffle = () => {
+  
+}
